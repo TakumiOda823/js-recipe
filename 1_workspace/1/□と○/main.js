1b4e94c62b7d73627f594eedffbc5208f66aae2d
TakumiOda823 <takumi99823@gmail.com>
2021-04-15 21:47:05 +0900

    大変でした

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -3,3 +3,8 @@ const figure = document.getElementById("figure")
 figure.onclick = function() {
   figure.classList.toggle("rounded")
 }
+
+const nicebutton = document.getElementById("nice-button")
+nicebutton.onclick = function() {
+  console.log = "こんにちわ"
+}
