d5d2a95b5a823aaf7edc3b8898ab300b7d0564b7
TakumiOda823 <takumi99823@gmail.com>
2021-04-16 13:38:33 +0900

    ×２倍にするやつがうまくいきません

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,10 +1,34 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
 
+//一つ減らす
+const display2 = document.getElementById("display2")
+const mainasuButton = document.getElementById("mainasu-button")
+
+//２倍にする
+const display3 = document.getElementById("display3")
+const nibaiButton = document.getElementById("nibai-button")
+
 let count = 0
+let count2 = 0
+let count3 = 0
 
 plusButton.onclick = function() {
   count += 1
 
   display.textContent = count
+}
+
+//一つ減らす
+mainasuButton.onclick = function() {
+  count2 -= 1
+
+  display2.textContent = count2
+}
+
+//２倍にする
+nibaiButton.onclick = function() {
+  count3 = count * 2
+
+  display3.textContent = count3
 }
