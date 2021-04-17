6512fe8cf236fc38a464b43f39e8840f41f26480
TakumiOda823 <takumi99823@gmail.com>
2021-04-17 12:01:35 +0900

    ゼミ

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,7 +1,35 @@
+// const genkiFunction = function(number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 3 === 0) {
+//       console.log(`${n}!!!!!!!`)
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+// genkiFunction(100)
+
+// const genkiFunction = function(number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 15 === 0) {
+//       console.log("FizzBuzz")
+//     } else if (n % 5 === 0) {
+//       console.log("Buzz")
+//     } else if (n % 3 === 0) {
+//       console.log("Fizz")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
+// genkiFunction(100)
+
 const genkiFunction = function(number) {
   for (let n = 1; n <= number; n++) {
     if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
+      console.log("${n}!!!!!!!")
     } else {
       console.log(n)
     }
@@ -9,3 +37,6 @@ const genkiFunction = function(number) {
 }
 
 genkiFunction(100)
+
+console.log(`${n}!!!!!!!`)
+console.log(`${n}!!!!!!!`)
