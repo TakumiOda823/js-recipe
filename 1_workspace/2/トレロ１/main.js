a1a1d392c5dbe34c990a9ed0bfdde8339fa3d983
TakumiOda823 <takumi99823@gmail.com>
2021-04-19 18:09:56 +0900

    trello難しい

--- 1_workspace/2/トレロ１/main.js
+++ 1_workspace/2/トレロ１/main.js
@@ -1,6 +1,42 @@
+"use strict"
+
 const inputElement = document.getElementById("input-todo")
 const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
+
+addButton.onclick = function() {
+  const card = createCard(inputElement.value)
+  //上記の関数の組み方の説明
+  container.append(card)
+
+  inputElement.value = ""
+  //上記の意味とは何なのか
+}
+
+const createCard = function() {
+  const card = document.createElement("div")
+  card.className = "card"
+
+  const todo = document.createElement("div")
+  todo.className = "todo"
+  todo.textContent = text
+  //ここのテキストの意味
+  card.append(todo)
+
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+
+  deleteButton.onclick = function() {
+    card.remove()
+  }
+  card.append(deleteButton)
+
+  return card
+  //ここのreturnの部分の説明をモット詳しくしてほしい
+  //カードを戻すとはどういうことなのか
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //俺が書いたやつ
 // addButton.onclick = function() {
@@ -26,37 +62,36 @@ const addButton = document.getElementById("add-button")
 // // 入力欄を空にする
 // inputElement.value = ""
 
-// 省略
-
+// 答案
 // 追加ボタンを押したら
-addButton.onclick = function() {
-  // 入力欄の値（テキスト）をとりだして、 text にいれる
-  const text = inputElement.value
+// addButton.onclick = function() {
+//   // 入力欄の値（テキスト）をとりだして、 text にいれる
+//   const text = inputElement.value
 
-  // card を作成
-  const card = document.createElement("div")
-  card.className = "card"
+//   // card を作成
+//   const card = document.createElement("div")
+//   card.className = "card"
 
-  // todo を作成
-  const todo = document.createElement("div")
-  todo.className = "todo"
-  todo.textContent = text
+//   // todo を作成
+//   const todo = document.createElement("div")
+//   todo.className = "todo"
+//   todo.textContent = text
 
-  // todo を card の中に追加する
-  card.append(todo)
+//   // todo を card の中に追加する
+//   card.append(todo)
 
-  // card を container の中に追加する
-  container.append(card)
+//   // card を container の中に追加する
+//   container.append(card)
 
-  // 入力欄を空にする
-  inputElement.value = ""
-}
+//   // 入力欄を空にする
+//   inputElement.value = ""
+// }
 
-const deleteButton = document.createElement("div")
-deleteButton.className = "delete"
+// const deleteButton = document.createElement("div")
+// deleteButton.className = "delete"
 
-deleteButton.onclick = function() {
-  card.remove()
+// deleteButton.onclick = function() {
+//   card.remove()
 
-  card.append(deleteButton)
-}
+//   card.append(deleteButton)
+// }
